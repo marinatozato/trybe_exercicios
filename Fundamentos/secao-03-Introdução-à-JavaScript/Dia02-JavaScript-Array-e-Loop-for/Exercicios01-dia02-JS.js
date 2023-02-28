add6e4ff91a62f887dfda61b461ca51d46473a22
Marina Tozato <marinatozato@gmail.com>
2023-02-28 20:40:55 -0300

    Exercicio 01 dia 02 JS Requisito 6

--- Fundamentos/secao-03-Introdução-à-JavaScript/Dia02-JavaScript-Array-e-Loop-for/Exercicios01-dia02-JS.js
+++ Fundamentos/secao-03-Introdução-à-JavaScript/Dia02-JavaScript-Array-e-Loop-for/Exercicios01-dia02-JS.js
@@ -1,17 +1,29 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 
+
+
+
+/* Utilizando for, descubra o maior valor contido no array e imprima-o.
+let higherNumber = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] > higherNumber) {
+        higherNumber = numbers[index]
+    }
+}
+console.log(higherNumber); */
+
+/* console.log(Math.max(...numbers)); */
 /* 
 
 //Percorra o array imprimindo todos os valores contidos nele com a função console.log().// 
-
 
 for (let index = 0; index < numbers.length; index += 1) {
     console.log(numbers[index]);
 } 
 
 */
-
 
 /* 
 
@@ -42,6 +54,9 @@ console.log(mediaAritimetica = soma / numbers.length);
 
 */
 
+
+/* Com base no código que acabou de gerar, faça com que: caso o valor final seja maior que 20, imprima a mensagem “Valor maior que 20”;
+ e, caso não seja maior que 20, imprima a mensagem “Valor menor ou igual a 20”.
 let soma = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
@@ -54,5 +69,4 @@ if (mediaAritimetica > 20) {
     console.log('Valor maior que 20');
 } else {
     console.log("Valor menor ou igual a 20");
-}
-
+} */
