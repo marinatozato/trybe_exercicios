9a6e23f06b5407e84653e689a116a7f7a5c2be59
Marina Tozato <marinatozato@gmail.com>
2023-02-28 15:51:16 -0300

    Para fixar 06 dia 02 JS

---
+++ Fundamentos/secao-03-Introdução-à-JavaScript/Dia02-JavaScript-Array-e-Loop-for/parafixar06JS.js
@@ -0,0 +1,26 @@
+
+/* let d1 = Math.ceil(Math.random() * 6);
+console.log('resultado d1:', d1);
+let d2 = Math.ceil(Math.random() * 6);
+
+while (d1 !== d2) {
+  d2 = Math.ceil(Math.random() * 6);
+  console.log('resultado d2:', d2);
+} */
+
+/* Utilizando o array abaixo, percorra-o somando todos os valores. Caso o valor final seja maior que 15, 
+imprima-o. Caso seja igual ou menor que 15, imprima a mensagem: “Valor menor que 16”: */
+
+/* let fruits = [3, 4, 10, 1, 12];
+
+let soma = 0;
+
+for (let index = 0; index < fruits.length; index += 1) {
+    soma += fruits[index];
+}
+
+if (soma > 15) {
+    console.log(soma);
+} else {
+    console.log('Valor menor que 16');
+} */
