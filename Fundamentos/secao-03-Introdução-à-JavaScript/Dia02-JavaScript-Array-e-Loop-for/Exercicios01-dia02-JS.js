a5b969b5ca961c3c40069bb6c87f3e06554d08d2
Marina Tozato <marinatozato@gmail.com>
2023-02-28 18:38:35 -0300

    Primeiro e Segundo Requisitos_Exercicio JS dia 02

---
+++ Fundamentos/secao-03-Introdução-à-JavaScript/Dia02-JavaScript-Array-e-Loop-for/Exercicios01-dia02-JS.js
@@ -0,0 +1,33 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+/* 
+
+//Percorra o array imprimindo todos os valores contidos nele com a função console.log().// 
+
+
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index]);
+} 
+
+*/
+
+
+/* 
+
+//Some todos os valores contidos no array e imprima o resultado.// 
+
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index]
+}
+console.log(soma);
+
+*/
+
+/* //Calcule e imprima a média aritmética dos valores contidos no array.
+
+A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.//
+
+*/
