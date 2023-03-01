f9130baadeb5b1d209cf5e6fa19d9e01df289944
Marina Tozato <marinatozato@gmail.com>
2023-02-28 21:13:11 -0300

    Exercicio 01 Requisito 06 JS dia 02

--- Fundamentos/secao-03-Introdução-à-JavaScript/Dia02-JavaScript-Array-e-Loop-for/Exercicios01-dia02-JS.js
+++ Fundamentos/secao-03-Introdução-à-JavaScript/Dia02-JavaScript-Array-e-Loop-for/Exercicios01-dia02-JS.js
@@ -3,6 +3,25 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 
 
+/* Descubra quantos valores ímpares existem no array e imprima o resultado. 
+Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
+let oddNumber = 0;
+
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !== 0) {
+      oddNumber += 1
+    }
+}
+
+if (oddNumber === 0) {
+    console.log('Nenhum valor ímpar encontrado');
+} else {
+    console.log(oddNumber);
+}   
+ */
+
+
 
 /* Utilizando for, descubra o maior valor contido no array e imprima-o.
 let higherNumber = numbers[0];
