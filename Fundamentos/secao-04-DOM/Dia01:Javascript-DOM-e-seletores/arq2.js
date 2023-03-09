f4e55e642c0c910cef84d949a541a2eb25df1a64
Marina Tozato <marinatozato@gmail.com>
2023-03-09 07:25:15 -0300

    Dia 01 Secao 04

---
+++ Fundamentos/secao-04-DOM/Dia01:Javascript-DOM-e-seletores/arq2.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("paragraph")[0].innerText = 'Primeiro parágrafo vamos falar sobre o livro'
+
+document.getElementsByClassName('title')[1].style.color = 'red'
---
+++ Fundamentos/secao-04-DOM/Dia01:Javascript-DOM-e-seletores/arquivoteste.js
@@ -0,0 +1,5 @@
+document.getElementById("page-title").innerText = "O Alquimista"
+
+document.getElementById("second-paragraph").innerText = "Esse livro é o melhor livro que já li, e por que?"
+
+document.getElementById("subtitle").innerText = 'Qual seu tipo de livro favorito?'
