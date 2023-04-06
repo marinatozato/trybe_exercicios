55561739464ce6d582b9634a69377663a1e88ea1
Marina Tozato <marinatozato@gmail.com>
2023-04-06 16:26:49 -0300

    Dia 04

--- Fundamentos/secao-07-HOFs/Dia01/Sorteador de loteria.js
+++
@@ -1,8 +0,0 @@
-const randomNumber = () => Math.floor(Math.random() * 5);
-
-const verifyNumber = (sortedNumber, callback) => sortedNumber === callback ? 'Parabéns, você ganhou' : 'Tente novamente';
-
-console.log(verifyNumber(1, randomNumber));
-
-
-console.log(randomNumber());
